c9295a97a4a9c723e49db9169a7e6a482e7636b6
DawidP2001 <150350371+DawidP2001@users.noreply.github.com>
2024-10-30 13:58:30 +0000

    Add files via upload

---
+++ FinalDemo/Fibonacci.java
@@ -0,0 +1,10 @@
+class Fibonacci {
+    public static void main(String[] args) {
+  
+      int[] arr = {1,1,2,3,5,8,13,21};
+      int result = 0;
+      for(int i=0;i<arr.length;i++){
+        result += arr[i];
+      }
+    }
+  }
